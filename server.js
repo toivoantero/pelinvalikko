a3d4d43a368523712296a3d7ad2e934e75cc2098
toivoantero <125696622+toivoantero@users.noreply.github.com>
2024-03-09 18:45:09 +0200

    muutoksia koska julkaisen eri palvelun kautta

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 const express = require('express');
-const path = require('path');
-
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, 'dist')));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+app.get('/api', (req, res) => {
+  res.send('Hello from the backend!');
 });
 
 app.listen(port, () => {
