865fe4af1568818e74dc5697be391fb403643fb3
toivoantero <125696622+toivoantero@users.noreply.github.com>
2024-03-11 22:02:39 +0200

    säätöjä

--- server.cjs
+++ server.cjs
@@ -2,8 +2,11 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get('/api', (req, res) => {
-  res.send('Hello from the backend!');
+app.use(express.static('dist')); // Olettaen, että index.html on dist-kansiossa
+
+// Pääsivun reititys
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 app.listen(port, () => {
