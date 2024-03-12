ff425c8b2ca893bf27e88c3d096eb9e84e64c929
toivoantero <125696622+toivoantero@users.noreply.github.com>
2024-03-12 16:04:52 +0200

    lisää reitityksiä

--- pelivalikkoServer.cjs
+++ pelivalikkoServer.cjs
@@ -39,6 +39,89 @@ app.get('/api/hahmo/all', (req, res) => {
     });
 });
 
+app.get('/api/omatvarusteet/all', (req, res) => {
+    db.all('select * from omatvarusteet', (error, result) => {
+        if (error) {
+            console.log(error.message);
+            return res.status(400).json({ message: error.message });
+        }
+
+        return res.status(200).json(result);
+    });
+});
+
+app.get('/api/kaupanvarusteet/all', (req, res) => {
+    db.all('select * from kaupanvarusteet', (error, result) => {
+        if (error) {
+            console.log(error.message);
+            return res.status(400).json({ message: error.message });
+        }
+
+        return res.status(200).json(result);
+    });
+});
+
+app.get('/api/hahmo/one/:id', (req, res) => {
+    let id = req.params.id;
+
+    db.get('select * from hahmo where id = ?', [id], (error, result) => {
+        if (error) {
+            console.log(error.message);
+            return res.status(400).json({ message: error.message });
+        }
+
+        // Jos haku ei tuottanut yhtään riviä
+        if (typeof (result) == 'undefined') {
+            return res.status(404).json({ message: 'Haettua hahmoa ei ole' });
+        }
+
+        return res.status(200).json(result);
+    });
+});
+
+app.get('/api/hahmo/kuvat', (req, res) => {
+    db.all('select kuva from hahmo where kuva IS NOT NULL', (error, result) => {
+        if (error) {
+            console.log(error.message);
+            return res.status(400).json({ message: error.message });
+        }
+
+        return res.status(200).json(result);
+    });
+});
+
+app.delete('/api/hahmo/delete/:id', (req, res) => {
+    let id = req.params.id;
+
+    // Huomaa, että ei nuolinotaatiofunktioina kuten muissa kohdissa
+    db.run('delete from hahmo where id = ?', [id], function (error) {
+        if (error) {
+            console.log(error.message);
+            return res.status(400).json({ message: error.message });
+        }
+
+        if (this.changes === 0) {
+            console.log('Ei poistettavaa');
+            return res.status(404).json({ message: 'Ei poistettavaa hahmoa' });
+        }
+
+        return res.status(200).json({ count: this.changes });
+    });
+});
+
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        callback(null, './kuvat'); // Mihin kansioon ladataan
+    },
+    filename: (req, file, callback) => {
+        callback(null, file.originalname);  // Millä tiedostonimellä
+    }
+});
+
+const upload = multer({ storage: storage })
+
 app.post('/api/hahmo/add', (req, res) => {
     // Lisää uusi hahmo tietokantaan
     const { nimi, ammatti, ika, kokemuspisteet, ase } = req.body;
@@ -54,7 +137,10 @@ app.post('/api/hahmo/add', (req, res) => {
     );
 });
 
-// Muut reititykset tarpeesi mukaan...
+app.get('/api/lataa/:nimi', (req, res) => {
+    let file = './kuvat/' + req.params.nimi;
+    res.download(file);
+});
 
 // Käsittely virheille
 app.use((err, req, res, next) => {
